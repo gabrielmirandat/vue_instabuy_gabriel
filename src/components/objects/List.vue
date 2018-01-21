11a5aeaf9f923ae21470afae4d7d6a3852a3d813
gabrielmirandat <gabrielmirandatt@gmail.com>
2018-01-21 21:57:40 -0200

    Mostrando dados relevantes na lista

--- src/components/objects/List.vue
+++ src/components/objects/List.vue
@@ -1,18 +1,24 @@
 <template>
   <v-card>
-    <v-list two-line>
+    <v-list three-line>
       <template v-for="item in items">
-        <v-subheader v-if="item.header" v-text="item.header" v-bind:key="item.title"/>
-        <v-divider v-else-if="item.divider" v-bind:inset="item.inset" v-bind:key="item.title"/>
-        <v-list-tile avatar v-else v-bind:key="item.title">
+        <v-list-tile avatar v-bind:key="item.id">
+
           <v-list-tile-avatar>
-            <img v-bind:src="item.avatar">
+            <img v-bind:src="item.imagem">
           </v-list-tile-avatar>
+
           <v-list-tile-content>
-            <v-list-tile-title v-html="item.title"></v-list-tile-title>
-            <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+            <v-list-tile-title v-html="item.nome"></v-list-tile-title>
+            <v-list-tile-sub-title v-html="item.marca"></v-list-tile-sub-title>
+            <v-list-tile-sub-title v-html="'R$ ' + item.preco"></v-list-tile-sub-title>
           </v-list-tile-content>
+
         </v-list-tile>
+
+        <v-divider v-if="item.id !== items[items.length-1].id"
+                   v-bind:inset="true" v-bind:key="item.nome"></v-divider>
+
       </template>
     </v-list>
   </v-card>
@@ -21,16 +27,9 @@
 <script>
 export default {
   name: 'list',
+  props: ['items'],
   data () {
     return {
-      items: [
-        { header: 'Today' },
-        { avatar: '/static/doc-images/lists/1.jpg', title: 'Brunch this weekend?', subtitle: "<span class='grey--text text--darken-2'>Ali Connors</span> — I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
-        { divider: true, inset: true },
-        { avatar: '/static/doc-images/lists/2.jpg', title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>', subtitle: "<span class='grey--text text--darken-2'>to Alex, Scott, Jennifer</span> — Wish I could come, but I'm out of town this weekend." },
-        { divider: true, inset: true },
-        { avatar: '/static/doc-images/lists/3.jpg', title: 'Oui oui', subtitle: "<span class='grey--text text--darken-2'>Sandra Adams</span> — Do you have Paris recommendations? Have you ever been?" }
-      ]
     }
   }
 }
