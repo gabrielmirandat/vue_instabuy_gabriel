7805e6caadfdcf57259f740b96b1e7255501f025
gabrielmirandat <gabrielmirandatt@gmail.com>
2018-01-21 19:43:39 -0200

    Adicionada Toolbar ao projeto

---
+++ src/components/objects/Toolbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-toolbar app manual-scroll color="indigo">
+    <v-spacer>
+      <v-toolbar-title>Produtos Instabuy</v-toolbar-title>
+    </v-spacer>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'toolbar'
+}
+</script>
