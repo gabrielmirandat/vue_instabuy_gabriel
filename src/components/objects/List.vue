c8882b6724141a47da0ad7839e0ae993c9983067
gabrielmirandat <gabrielmirandatt@gmail.com>
2018-01-21 19:43:54 -0200

    Adicionada componente de Lista ao projeto

---
+++ src/components/objects/List.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card>
+    <v-list two-line>
+      <template v-for="item in items">
+        <v-subheader v-if="item.header" v-text="item.header" v-bind:key="item.title"/>
+        <v-divider v-else-if="item.divider" v-bind:inset="item.inset" v-bind:key="item.title"/>
+        <v-list-tile avatar v-else v-bind:key="item.title">
+          <v-list-tile-avatar>
+            <img v-bind:src="item.avatar">
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title v-html="item.title"></v-list-tile-title>
+            <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </template>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  data () {
+    return {
+      items: [
+        { header: 'Today' },
+        { avatar: '/static/doc-images/lists/1.jpg', title: 'Brunch this weekend?', subtitle: "<span class='grey--text text--darken-2'>Ali Connors</span> — I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
+        { divider: true, inset: true },
+        { avatar: '/static/doc-images/lists/2.jpg', title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>', subtitle: "<span class='grey--text text--darken-2'>to Alex, Scott, Jennifer</span> — Wish I could come, but I'm out of town this weekend." },
+        { divider: true, inset: true },
+        { avatar: '/static/doc-images/lists/3.jpg', title: 'Oui oui', subtitle: "<span class='grey--text text--darken-2'>Sandra Adams</span> — Do you have Paris recommendations? Have you ever been?" }
+      ]
+    }
+  }
+}
+</script>
